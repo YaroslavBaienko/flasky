cbbc039de68b6ef4296c7473475493d735d39d97
Yaroslav Baienko <zerhug@gmail.com>
2022-11-22 21:29:37 +0200

    my first flask app

--- flasky.py
+++ flasky.py
@@ -1,7 +1,19 @@
+import flask
 from flask import (
     Flask,
     request,
     make_response
 )
 
+app = flask.Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return "Hello world!"
+
+
+@app.route()
+
+if __name__ == '__main__':
+    app.run()
